f07b20514a4c87d6066a30cd78247b696b2fede4
jungwoo <rlawjddn103@naver.com>
2022-06-27 17:21:25 +0900

    Add DP BOJ2293

---
+++ PYTHON_BOJ/DP/BOJ2293.py
@@ -0,0 +1,18 @@
+import sys
+
+n,k = map(int,sys.stdin.readline().rstrip().split())
+
+dp = [0 for _ in range(0,k+1)]
+coin = []
+
+for _ in range(0,n):
+    coin.append(int(sys.stdin.readline().rstrip()))
+
+dp[0]=1
+
+for i in coin:
+    for j in range(1, k + 1):
+        if j - i >= 0:
+            dp[j] += dp[j - i]
+
+print(dp)
