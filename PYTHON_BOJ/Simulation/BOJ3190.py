de39b9365412e5c7cf176d2d9634d1878a86856c
jungwoo <rlawjddn103@naver.com>
2022-06-27 14:02:54 +0900

    Solved Simulation BOJ3190

--- PYTHON_BOJ/Simulation/BOJ3190.py
+++ PYTHON_BOJ/Simulation/BOJ3190.py
@@ -27,39 +27,47 @@ for _ in range(0,L):
     temp = sys.stdin.readline().rstrip().split()
     direct.append((int(temp[0]),temp[1]))
 
-time = 1
+time = 0
 
 while True:
-    if time == direct[0][0]:
-        if direct[0][1] == 'D':
-            now_direct_idx += 1
-            if now_direct_idx > 3:
-                now_direct_idx -= 4
-        else:
-            now_direct_idx -= 1
-            if now_direct_idx < 0:
-                now_direct_idx += 4
-            
-        direct.popleft()
+
+    time += 1
     
     head[0] += ny[now_direct_idx]
     head[1] += nx[now_direct_idx]
-    snake.append(head)
 
-    print(head)
+    snake.append([head[0],head[1]])
+    #print(snake)
+    
     if head[0] >= N or head[0] < 0 or head[1] >= N or head[1] < 0:
         break
     if board[head[0]][head[1]] == 2:
         break
-
-    if board[head[0]][head[1]] == 1:
-        board[head[0]][head[1]] = 2
-        y,x = snake.popleft()
-        board[y][x] = 0
-        print('check')
-    else:
-        board[head[0]][head[1]] = 2
     
-    time += 1
+    if board[head[0]][head[1]] == 0:
+       
+        y,x = snake.popleft()
+        
+        #print("y:"+str(y)+" x:"+str(x))
+        board[y][x] = 0
+
+    board[head[0]][head[1]] = 2
+        
+    #print(head)
+    #print(board)
+    
+
+    if len(direct) > 0:
+        if time == direct[0][0]:
+            if direct[0][1] == 'D':
+                now_direct_idx += 1
+                if now_direct_idx > 3:
+                    now_direct_idx -= 4
+            else:
+                now_direct_idx -= 1
+                if now_direct_idx < 0:
+                    now_direct_idx += 4
+            
+            direct.popleft()
 
 print(time)
