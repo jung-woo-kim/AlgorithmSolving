62d227edccb01c41e89dad020e230b367584d844
jungwookim <rlawjddn103@naver.com>
2022-07-15 19:19:50 +0900

    Solved Pro 뉴스클러스터링

---
+++ PYTHON_Programmers/뉴스_클러스터링.py
@@ -0,0 +1,66 @@
+def solution(str1, str2):
+    answer = 0
+    str1_dic = dict()
+    str2_dic = dict()
+    total_dic = dict()
+    same_dic = dict()
+    
+    for i in range(0,len(str1)-1):
+        if not('a' <= str1[i] <= 'z' or 'A' <= str1[i] <= 'Z'):
+            continue
+        if not('a' <= str1[i+1] <= 'z' or 'A' <= str1[i+1] <= 'Z'):
+            continue
+        temp = str1[i]+str1[i+1]
+        temp = temp.lower()
+        try:
+            str1_dic[temp] += 1
+        except:
+            str1_dic[temp] = 1
+            
+        try:
+            total_dic[temp] += 1
+        except:
+            total_dic[temp] = 1
+            
+    for i in range(0,len(str2)-1):
+        if not('a' <= str2[i] <= 'z' or 'A' <= str2[i] <= 'Z'):
+            continue
+        if not('a' <= str2[i+1] <= 'z' or 'A' <= str2[i+1] <= 'Z'):
+            continue
+        temp = str2[i]+str2[i+1]
+        temp = temp.lower()
+        try:
+            str2_dic[temp] += 1
+        except:
+            str2_dic[temp] = 1
+            
+        try:
+            total_dic[temp] += 1
+        except:
+            total_dic[temp] = 1
+                
+    
+    for item in str1_dic.keys():
+        try:
+            str2_dic[item] += 0
+            same_dic[item] = min(str1_dic[item],str2_dic[item])
+        except:
+            pass
+    print(total_dic)
+    print(same_dic) 
+    if (len(str2_dic) == 0 and len(str1_dic) == 0):
+        answer = 1 *65536
+        return answer
+    
+    
+    
+    t_sum = 0
+    for key in total_dic.keys():
+        t_sum += total_dic[key]
+    
+    s_sum = 0
+    for key in same_dic.keys():
+        s_sum += same_dic[key]
+    
+    answer = int((s_sum / (t_sum-s_sum)) * 65536)
+    return answer
