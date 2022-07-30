390dde886512cf5526681c5b9a2b8ec73f17b4f8
jungwoo <rlawjddn103@naver.com>
2022-07-30 14:18:57 +0900

    Solved BackTracking BOJ1062

--- PYTHON_BOJ/BackTracking/BOJ1062.py
+++ PYTHON_BOJ/BackTracking/BOJ1062.py
@@ -3,59 +3,44 @@ import sys
 N, K = map(int,sys.stdin.readline().rstrip().split())
 
 li = []
-alpha = []
 answer = 0
 antatica = 0
+
+visited = [False for _ in range(26)]
+antic = ['a','n','t','i','c']
+
+for w in antic:
+    visited[ord(w) -97] = True
+
 for _ in range(N):
     word = sys.stdin.readline().rstrip()[4:-4]
-    
-    word = list(set(list(word)))
-    word.sort()
-    temp = []
-    for w in word:
-        if w not in 'antic':
-            temp.append(w)
-            alpha.append(w)
-    if len(temp) == 0:
-        antatica += 1
-    else:
-        temp.sort()
-        li.append("".join(temp))
+    word = set(list(word)) - set(antic)
+    li.append(word)
 
-alpha = list(set(alpha))
-alpha.sort()
 
-print(alpha)
-
-visited = [False for i in range(len(alpha))]
-
-def dfs(depth,n,s):
+def dfs(depth,s,n):
     global answer
-
     if depth == K-5:
-        print(n)
-        temp = antatica
-        for word in li:
-            if word in n:
-                temp +=1
+        
+        teach = set(list(n))
+        temp = 0
+        for w in li:
+            t = w - teach
+            if len(t) == 0:
+                temp += 1
         answer = max(answer,temp)
         return
+    
+    for i in range(s,26):
+        if not visited[i]:
+            visited[i] = True
+            temp = n+chr(i+97)
+            dfs(depth + 1,i,temp)
+            visited[i] = False
 
-    for i in range(s,len(alpha)):
-        temp = n+alpha[i]
-        dfs(depth+1,temp,i+1)
-
-if len(alpha) <= K-5:
-    print(N)
-    exit()
+if K >= 5:
+    dfs(0,0,"")
+    print(answer)
 
 if K < 5:
     print(0)
-    exit()
-
-if K == 5:
-    print(antatica)
-    exit()
-
-dfs(0,"",0)
-print(answer)
