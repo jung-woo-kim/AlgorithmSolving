bd568429014feda9d038342a7e77cb7faf9d73f5
jungwoo <rlawjddn103@naver.com>
2022-08-14 15:31:26 +0900

    Solved DisjointSet BOJ1717

---
+++ PYTHON_BOJ/DisjointSet/BOJ1717.py
@@ -0,0 +1,29 @@
+import sys
+
+N, M = map(int,sys.stdin.readline().rstrip().split())
+
+parent = [i for i in range(N+1)]
+
+def find(parent,x):
+    if x != parent[x]:
+        parent[x] = find(parent,parent[x])
+    return parent[x]
+
+def union(parent,x,y):
+    x = find(parent,x)
+    y = find(parent,y)
+    if x < y:
+        parent[y] = x
+    else:
+        parent[x] = y
+    
+for _ in range(M):
+    z, a, b = map(int, input().split())
+    print(parent)
+    if z == 0 :
+        union(parent, a, b)
+    else :
+        if find(parent, a) == find(parent, b):
+            print('YES')
+        else :
+            print('NO')
