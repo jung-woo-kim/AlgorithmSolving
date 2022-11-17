321d782d2bd461979d6e2e33942b6e931fd176ca
JungWoo <rlawjddn103@naver.com>
2022-11-17 16:17:22 +0900

    Solved DP BOJ3067

--- PYTHON_BOJ/DP/BOJ3067.py
+++ PYTHON_BOJ/DP/BOJ3067.py
@@ -4,17 +4,15 @@ T = int(sys.stdin.readline().rstrip())
 
 for _ in range(T):
     N = int(sys.stdin.readline().rstrip())
-    coins = list(map(int,sys.stdin.readline().rstrip().split()))
-    money = int(sys.stdin.readline().rstrip())
+    coin = list(map(int,sys.stdin.readline().rstrip().split()))
+    M = int(sys.stdin.readline().rstrip())
+    dp = [0 for _ in range(M+1)]
+    dp[0] = 1
+    for i in coin:
+        for j in range(1, M + 1):
+            if j - i >= 0:
+                dp[j] += dp[j - i]
 
-    dp = [[0 for _ in range(money+1)]for __ in range(N)]
-    for i in range(money+1):
-        dp[0][i] = 1
+    print(dp[M])
 
-    for i in range(1,N):
-        for j in range(money+1):
-            dp[i][j] += dp[i-1][j]
-            if j-coins[i] >= 0:
-                dp[i][j] += dp[i-1][j-coins[i]]
 
-    print(dp)  
