20bc11fe464c9d329b6eab24801b1e5a35146c67
JungWoo <rlawjddn103@naver.com>
2023-06-15 00:43:35 +0900

    add programmers 혼자 놀기의 장인

--- PYTHON_Programmers/혼자_놀기의_장인.py
+++ PYTHON_Programmers/혼자_놀기의_장인.py
@@ -3,15 +3,15 @@ def solution(cards):
     visited = [False for i in range(len(cards))]
 
     for i in range(len(cards)):
-        temp = 1
+        temp = 0
         idx = i
-        while visited[cards[idx]-1]:
+        while not visited[cards[idx]-1]:
             visited[cards[idx]-1] = True
-            temp *= cards[idx]
-            idx = cards[idx]
-        answer.append(temp)
+            temp += 1
+            idx = cards[idx] -1
+        if temp != 0:
+            answer.append(temp)
     answer.sort(reverse=True)
-    print(answer)
     if len(answer) == 1:
         return 0
     return answer[0] * answer[1]
